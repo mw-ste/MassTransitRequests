8bcaaf5841fb15abdd8b64a955adc71e1c8925ae
Michael Stenger <michael.stenger@outlook.de>
2022-03-10 17:44:25 +0100

    adding loggers

--- ContextA/Infrastructure/ExampleRequestHandler.cs
+++ ContextA/Infrastructure/ExampleRequestHandler.cs
@@ -1,11 +1,19 @@
 ﻿using ContextA.Contracts;
 using Infrastructure;
+using Microsoft.Extensions.Logging;
 
 namespace ContextA.Infrastructure;
 
 public class ExampleRequestHandler 
     : MassTransitRequestHandler<ExampleRequest, ExampleResponse>
 {
+    private readonly ILogger<ExampleRequestHandler> _logger;
+
+    public ExampleRequestHandler(ILogger<ExampleRequestHandler> logger)
+    {
+        _logger = logger;
+    }
+
     public override Task<ExampleResponse> HandleQuery(ExampleRequest request)
     {
         var response = new ExampleResponse(request.RequestId, string.Join("", request.RequestContent.Reverse()));
--- WebApi/Controllers/RequestsController.cs
+++ WebApi/Controllers/RequestsController.cs
@@ -1,18 +1,22 @@
-namespace WebApi.Controllers;
-
 using ContextA.Contracts;
 using Microsoft.AspNetCore.Mvc;
 using Shared;
+
+namespace WebApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class RequestsController : ControllerBase
 {
     private readonly IRequester<ExampleRequest, ExampleResponse> _requester;
+    private readonly ILogger<RequestsController> _logger;
 
-    public RequestsController(IRequester<ExampleRequest, ExampleResponse> requester)
+    public RequestsController(
+        IRequester<ExampleRequest, ExampleResponse> requester,
+        ILogger<RequestsController> logger)
     {
         _requester = requester;
+        _logger = logger;
     }
 
     [HttpGet(Name = nameof(Get))]
